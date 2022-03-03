54c51f47d81a0fb884cd231d1a3317a00996f222
Andrew Northall <andrew@northall.me.uk>
2022-03-03 16:39:35 +0000

    Update list_display for most models

--- app/database/admin.py
+++ app/database/admin.py
@@ -1,10 +1,45 @@
+from csv import list_dialects
 from django.contrib import admin
 
 from .models import Cave, System, District, Club, CavingBody, Country
 
 admin.site.register(Cave)
 admin.site.register(System)
-admin.site.register(District)
-admin.site.register(Club)
-admin.site.register(CavingBody)
-admin.site.register(Country)
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "code",
+    )
+
+
+@admin.register(CavingBody)
+class CavingBodyAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "district",
+        "country",
+        "website",
+    )
+
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "location",
+        "district",
+        "caving_body",
+    )
+
+
+@admin.register(District)
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "code",
+        "country",
+        "website",
+    )
