0120df0bad80eeb5a5ff870233ee1f58173cecb7
Andrew Northall <andrew@northall.me.uk>
2022-03-04 18:49:37 +0000

    Add base views

--- app/database/urls.py
+++ app/database/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="db-index"),
+    path("caves/", views.caves, name="db-caves"),
+    path("systems/", views.systems, name="db-systems"),
+    path("clubs/", views.clubs, name="db-clubs"),
 ]
--- app/database/views.py
+++ app/database/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "base.html")
+
+
+def caves(request):
+    return render(request, "base.html")
+
+
+def systems(request):
+    return render(request, "base.html")
+
+
+def clubs(request):
+    return render(request, "base.html")
