bec3be64760801e3ca4deece1f36e695a33f8148
Andrew Northall <andrew@northall.me.uk>
2022-03-05 20:39:40 +0000

    Add basic homepage view

--- app/core/views.py
+++ app/core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from database.models import Cave, CaveSystem
 
 
 def index(request):
-    return render(request, "base.html")
+    recent = Cave.objects.all().order_by("-updated_on")[:20]
+    context = {"recent_updates": recent}
+    return render(request, "homepage.html", context)
