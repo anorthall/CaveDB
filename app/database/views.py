7fc2db2610e9c94467664486882e97b07b11d91e
Andrew Northall <andrew@northall.me.uk>
2022-03-10 12:18:25 +0000

    Vastly improve query performance

--- app/database/views.py
+++ app/database/views.py
@@ -7,17 +7,49 @@ from .models import Region, Cave, CaveSystem, Country
 
 def index(request):
     query = QuerySetSequence(Cave.objects.all(), CaveSystem.objects.all())
-    recent_updates = query.order_by("updated_on")[:10]
+    recent_updates = query.order_by("updated_on")[:10].select_related(
+        "region",
+        "region__country",
+    )
+    regions = (
+        Region.objects.all()
+        .select_related(
+            "country",
+        )
+        .prefetch_related(
+            "cavesystem_set",
+            "cave_set",
+        )
+    )
 
     context = {
         "recent": recent_updates,
-        "regions": Region.objects.all(),
+        "regions": regions,
     }
     return render(request, "dbindex.html", context)
 
 
 def region(request, slug):
-    context = {"region": get_object_or_404(Region, slug=slug)}
+    region = get_object_or_404(Region, slug=slug)
+    caves = region.cave_set.all().select_related(
+        "organisation",
+        "system",
+    )
+    systems = (
+        region.cavesystem_set.all()
+        .select_related(
+            "organisation",
+        )
+        .prefetch_related(
+            "cave_set",
+        )
+    )
+
+    context = {
+        "region": region,
+        "caves": caves,
+        "systems": systems,
+    }
     return render(request, "region.html", context)
 
 
@@ -30,8 +62,12 @@ def cave(request, slug):
 
 
 def system(request, slug):
+    cave = get_object_or_404(CaveSystem, slug=slug)
+    subcaves = cave.cave_set.all().select_related("region")
+
     context = {
-        "cave": get_object_or_404(CaveSystem, slug=slug),
+        "cave": cave,
+        "caves_in_system": subcaves,
         "type": "system",
     }
     return render(request, "cave.html", context)
@@ -39,10 +75,31 @@ def system(request, slug):
 
 def country(request, slug):
     country = get_object_or_404(Country, slug=slug)
+
+    systems = CaveSystem.objects.none()
+    caves = Cave.objects.none()
+    for region in country.region_set.all():
+        caves = caves.union(
+            region.cave_set.all().select_related(
+                "organisation",
+                "region",
+                "system",
+            )
+        )
+
+        systems = systems.union(
+            region.cavesystem_set.all()
+            .select_related(
+                "organisation",
+                "region",
+            )
+            .prefetch_related("cave_set")
+        )
+
     context = {
         "country": country,
-        "systems": country.cavesystems(),
-        "caves": country.caves(),
+        "systems": systems,
+        "caves": caves,
     }
 
     if len(context["caves"]) == 0 and len(context["systems"]) == 0:
@@ -77,6 +134,11 @@ def search(request):
     # Search name and location
     st = search_terms
     results = query.filter(Q(name__icontains=st) | Q(location__iexact=st))
+    results = results.select_related(
+        "organisation",
+        "region",
+        "region__country",
+    )
 
     # If only one result, and it's a Cave or CaveSystem, just redirect to the page
     if len(results) == 1:
